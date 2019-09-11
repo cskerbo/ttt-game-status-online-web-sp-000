50e32d507cedad424bef6ee3fc9780856fac28b5
Chris Kerbo <chris.kerbo@outlook.com>
2019-09-11 16:58:54 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,8 +18,8 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
     win_index_1 = WIN_COMBINATIONS[0]
-    win_index_1 = WIN_COMBINATIONS[1]
-    win_index_1 = WIN_COMBINATIONS[2]
+    win_index_2 = WIN_COMBINATIONS[1]
+    win_index_3 = WIN_COMBINATIONS[2]
 
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
