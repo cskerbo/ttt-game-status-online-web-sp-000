69bca86d18efd8cc99f7d261901c30116b67893b
Chris Kerbo <chris.kerbo@outlook.com>
2019-09-11 16:35:54 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,28 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+
+]
+
+def won?(board)
+  win_index_1 = WIN_COMBINATIONS[0]
+  win_index_1 = WIN_COMBINATIONS[1]
+  win_index_1 = WIN_COMBINATIONS[2]
+
+  position_1 = board[win_index_1]
+  position_2 = board[win_index_2]
+  position_3 = board[win_index_3]
+
+  if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return won?(board)
+  end
+end
